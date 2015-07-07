e10c88b5cbabc2f52aacd2402e7ca31c7b08a311
boggogo <koemdjiev@gmail.com>
2015-07-07 10:30:17 +0100

    speed of timer is decreased now

--- app/src/main/java/koemdzhiev/com/mathmadness/MainActivity.java
+++ app/src/main/java/koemdzhiev/com/mathmadness/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView digitTwo;
     private TextView mMathOperator;
     private TextView score;
-    private TextView sum;
+    private TextView mSum;
     private ImageView mTrueBtn;
     private ImageView mFalseBtn;
     private ProgressBar mProgressBar;
@@ -33,10 +33,13 @@ public class MainActivity extends AppCompatActivity {
     private long timer_length = 10*1000;
     private long timer_interval = 1;
     private int consecutiveGames = 0;
-    Random ifTrue;
+    private Random ifTrue;
     private int firstNumber = 0;
     private int secondNumber = 0;
-    private int sum2 = 0;
+    private int sum = 0;
+    private long totalMillisUntilFinished = 0;
+    private boolean firstTime = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         mMathOperator = (TextView)findViewById(R.id.mathOperator);
         score = (TextView)findViewById(R.id.score);
         score.setText("Score: "+consecutiveGames);
-        sum = (TextView)findViewById(R.id.sum);
+        mSum = (TextView)findViewById(R.id.sum);
         mTrueBtn = (ImageView)findViewById(R.id.trueButton);
         mFalseBtn = (ImageView)findViewById(R.id.falseButton);
         mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
@@ -71,9 +74,24 @@ public class MainActivity extends AppCompatActivity {
                     score.setText("Score: "+consecutiveGames);
                     //Toast.makeText(MainActivity.this,"Correct!",Toast.LENGTH_SHORT).show();
                     generateMathProblem();
-//                    if(timer_length > 5000){
-//                        timer_length -=1000;
-//                    }
+                    if(timer_length > 5000){
+                        timer_length-= 1000;
+                    }
+                    if(consecutiveGames == 30){
+                        timer_length -=1000;
+                    }
+                    if(consecutiveGames == 50){
+                        timer_length -=1000;
+                    }
+                    if(consecutiveGames == 60){
+                        timer_length -=1000;
+                    }
+                    if(consecutiveGames == 80){
+                        timer_length -=1000;
+                    }
+                    if(consecutiveGames == 100){
+                        timer_length -=2000;
+                    }
                     mCountDownTimer.cancel();
                     createNStartTimer();
                 }else{
@@ -101,9 +119,9 @@ public class MainActivity extends AppCompatActivity {
                     score.setText("Score: "+consecutiveGames);
                     //Toast.makeText(MainActivity.this,"Correct!",Toast.LENGTH_SHORT).show();
                     generateMathProblem();
-//                    if(timer_length > 5000){
-//                        timer_length -=1000;
-//                    }
+                    if(timer_length > 5000){
+                        timer_length-= 1000;
+                    }
                     mCountDownTimer.cancel();
                     createNStartTimer();
                 }else{
@@ -178,21 +196,21 @@ public class MainActivity extends AppCompatActivity {
             while(!equasionIsTrue){
                 firstNumber = new Random().nextInt(10)+1;
                  secondNumber = new Random().nextInt(10)+1;
-                 sum2 = new Random().nextInt(10)+1;
+                 sum = new Random().nextInt(10)+1;
 
                 switch (generateRandomMathOperator){
                     case 0:total = firstNumber + secondNumber;break;
                     case 1:total = firstNumber - secondNumber;break;
                     case 2:total = firstNumber * secondNumber;break;
                 }
-                boolean isItTrue = total == sum2;
+                boolean isItTrue = total == sum;
                 if(isItTrue == true){
                     equasionIsTrue = true;
                 }
             }
             digitOne.setText(firstNumber+"");
             digitTwo.setText(secondNumber+"");
-            sum.setText(sum2+"");
+            mSum.setText(sum + "");
         }
 
         else{
@@ -209,28 +227,28 @@ public class MainActivity extends AppCompatActivity {
             while(equasionIsTrue){
                 firstNumber = new Random().nextInt(10)+1;
                 secondNumber = new Random().nextInt(10)+1;
-                sum2 = new Random().nextInt(10)+1;
+                sum = new Random().nextInt(10)+1;
 
                 switch (generateRandomMathOperator){
                     case 0:total = firstNumber + secondNumber;break;
                     case 1:total = firstNumber - secondNumber;break;
                     case 2:total = firstNumber * secondNumber;break;
                 }
-                boolean isItTrue = total == sum2;
+                boolean isItTrue = total == sum;
                 if(isItTrue == false){
                     equasionIsTrue = false;
                 }
             }
             digitOne.setText(firstNumber+"");
             digitTwo.setText(secondNumber+"");
-            sum.setText(sum2+"");
+            mSum.setText(sum + "");
         }
 
     }
     private void transferUserToStartScreen() {
         //Toast.makeText(MainActivity.this,"Incorrect!",Toast.LENGTH_SHORT).show();
        String toBeSend = digitOne.getText().toString() + mMathOperator.getText().toString()+
-        digitTwo.getText().toString()+ "="+sum.getText().toString();
+        digitTwo.getText().toString()+ "="+ mSum.getText().toString();
         Intent intent = new Intent(MainActivity.this,StartActivity.class);
         intent.putExtra("score", toBeSend);
         startActivity(intent);
@@ -247,9 +265,8 @@ public class MainActivity extends AppCompatActivity {
         mCountDownTimer = new CountDownTimer(timer_length,timer_interval) {
             @Override
             public void onTick(long millisUntilFinished) {
-                //Log.d(TAG, "Mil until finish:" + millisUntilFinished);
-                //if(firstTime){totalMillisUntilFinished = millisUntilFinished; firstTime = false;}
-                int progress = (int) (millisUntilFinished/100);
+                if(firstTime){totalMillisUntilFinished = millisUntilFinished; firstTime = false;}
+                int progress = (int) (millisUntilFinished*100/totalMillisUntilFinished);
                 //Log.d(TAG, "progressBar:" + progress);
                 mProgressBar.setProgress(progress);
             }
